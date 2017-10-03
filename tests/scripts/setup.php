27b8c4e0249994ed20f27e86d78928558e1fa65f
Daniël Klabbers <daniel@klabbers.email>
2017-10-03 05:06:09 +0200

    possible pgsql fix and setup script

--- tests/scripts/setup.php
+++ tests/scripts/setup.php
@@ -12,43 +12,22 @@
  * @see https://github.com/hyn/multi-tenant
  */
 
-chdir(getenv('CI_PROJECT_DIR'));
-
-$base_path = __DIR__ . '/../../';
-
 if (getenv('TRAVIS_BUILD_DIR')) {
     putenv('CI_PROJECT_DIR=' . getenv('TRAVIS_BUILD_DIR'));
 }
 
-/**
- * Install correct version of laravel.
- * Install db driver dependencies.
- */
-if (preg_match(
-    '/^(?<webserver>[a-z]+)\-(?<php_version>[0-9\.]+)\-(?<db>[a-z]+)$/',
-    getenv('CI_JOB_NAME'),
-    $m
-)) {
-    putenv("BUILD_WEBSERVER={$m['webserver']}");
-    putenv("BUILD_PHP_VERSION={$m['php_version']}");
+chdir(getenv('CI_PROJECT_DIR'));
 
-    echo <<<EOM
-    
-    
-Found advanced CI configuration from CI_JOB_NAME environment variable:
-    - Webserver {$m['webserver']}
-    - PHP {$m['php_version']}
-    - Db driver: {$m['db']}
+$base_path = __DIR__ . '/../../';
 
-
-EOM;
-
-    foreach ([
-                 "$base_path/vendor/laravel/laravel/config/tenancy.php",
-                 "$base_path/vendor/laravel/laravel/config/webserver.php",
-             ] as $config) {
-        if (file_exists($config)) {
-            @unlink($config);
-        }
+if (getenv('TRAVIS_PHP_VERSION')) {
+    putenv('BUILD_PHP_VERSION=' . getenv('TRAVIS_PHP_VERSION'));
+}
+foreach ([
+             "$base_path/vendor/laravel/laravel/config/tenancy.php",
+             "$base_path/vendor/laravel/laravel/config/webserver.php",
+         ] as $config) {
+    if (file_exists($config)) {
+        @unlink($config);
     }
 }
